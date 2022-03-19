ec82714d385ab254f215553fa3cc09013df245cb
GhostMaster69-dev <rathore6375@gmail.com>
2022-03-19 10:29:16 +0530

    vince: Drop Mi Sound Enhancer
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -183,10 +183,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.1-service
 
-# Dirac
-PRODUCT_PACKAGES += \
-    XiaomiDirac
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
